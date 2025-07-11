e054821ffacb1458af1b2b1f3a328820121645e3
Pradyumn Shirsath <pradyumn17.iiitn@gmail.com>
2025-07-12 02:26:45 +0530

    testing 2

--- Extension_backend/api/index.js
+++ Extension_backend/api/index.js
@@ -1,33 +1,15 @@
 import express from "express";
+import serverless from "serverless-http";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-// import handler from "./emailer.js"; // adjust path if needed
-
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
-app.use(express.json()); // to parse JSON request body
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // or restrict to your extension origin
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Lund" });
 });
 
-
-// app.post("/api/todos", async (req, res) => {
-//   await handler(req, res);
-// });
-app.get("/", async (req, res) => {
-  res.json({
-    msg: "Lund"
-  });
-});
-
-export default app;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
+export const handler = serverless(app);
